0bbfec4b224e1ff6fdcf1cf58813ad11d3c83875
AmishAlvi <amish.alvi@gmail.com>
2022-07-06 22:31:03 +0500

    working laser beam
    laser beam script works and casts a laser using a raycast and a line renderer

---
+++ Assets/Scripts/LaserBeam.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserBeam
+{
+    Vector3 pos, dir;
+
+    GameObject laserObj;
+    LineRenderer laser;
+    List<Vector2> laserIndices = new List<Vector2>();
+
+
+    public LaserBeam(Vector2 pos, Vector2 dir, Material material)
+    {
+        this.laser = new LineRenderer();
+        this.laserObj = new GameObject();
+        this.laserObj.name = "Laser Beam";
+        this.pos = pos;
+        this.dir = dir;
+
+        this.laser = this.laserObj.AddComponent(typeof(LineRenderer)) as LineRenderer;
+        this.laser.startWidth = 0.1f;
+        this.laser.endWidth = 0.1f;
+        this.laser.material = material;
+        this.laser.startColor = Color.red;
+        this.laser.endColor = Color.red;
+
+        CastRay(pos, dir, laser);
+    }
+
+    void CastRay(Vector2 pos, Vector2 dir, LineRenderer laser)
+    {
+        laserIndices.Add(pos);
+
+        Ray ray = new Ray(pos, dir);
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit, 30, 1))
+        {
+            laserIndices.Add(hit.point);
+            UpdateLaser();
+        }
+        else
+        {
+            laserIndices.Add(ray.GetPoint(30));
+            UpdateLaser();
+        }
+    }
+
+    void UpdateLaser()
+    {
+        int count = 0;
+        laser.positionCount = laserIndices.Count;
+
+        foreach(Vector2 idx in laserIndices)
+        {
+            laser.SetPosition(count, idx);
+            count++;
+        }
+    }
+}
